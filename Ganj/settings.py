4e2ca5597717fb456a42a30f9979779e504559aa
BGOPC <bb8933400@gmail.com>
2023-04-19 17:34:19 +0330

    ..

--- Ganj/settings.py
+++ Ganj/settings.py
@@ -57,7 +57,9 @@ EMAIL_PORT = '2525'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
