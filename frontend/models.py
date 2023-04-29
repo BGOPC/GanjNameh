233da1e8e942b21c0485dae71bdec8342146dc7d
BGOPC <bb8933400@gmail.com>
2023-04-29 21:11:38 +0330

    ...

--- frontend/models.py
+++ frontend/models.py
@@ -4,7 +4,6 @@ from users.models import User
 
 # Create your models here.
 
-
 class Booklet(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, null=False, name="Booklet Name")
