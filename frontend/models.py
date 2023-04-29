d95b3e6a858714d6a3499a4f31310ae9db0a85e1
BGOPC <bb8933400@gmail.com>
2023-04-29 21:04:50 +0330

    Booklet model

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import User
+
 
 # Create your models here.
+
+
+class Booklet(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=False, name="Booklet Name")
+    category = models.CharField(max_length=50, null=False, default="Subject")
+    description = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
