f3e586c6baa90b5715a3666164218d6e7c0ec543
BGOPC <bb8933400@gmail.com>
2023-04-29 22:00:15 +0330

    Added a model for Journal, Added FileField to Booklet

--- frontend/models.py
+++ frontend/models.py
@@ -10,3 +10,24 @@ class Booklet(models.Model):
     category = models.CharField(max_length=50, null=False, default="Subject")
     description = models.TextField(null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
+    booklet_file = models.FileField(upload_to='booklets/')
+
+    def __str__(self):
+        return self.name
+
+
+class Journal(models.Model):
+    author = 1
+    name = models.CharField(max_length=100, null=False, name="Booklet Name")
+    category = models.CharField(max_length=50, choices=[
+        "Physics",
+        "Movie and comic",
+        "Hello copter",
+        "Biology and Chemistry",
+        "Computer",
+        "Philosophy",
+        "Writing",
+    ], null=False, default="Subject")
+    description = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    booklet_file = models.FileField(upload_to='journals/')
