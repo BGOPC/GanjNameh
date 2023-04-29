b493aaf74611e1d7bfa0b1802948bccbd9c535ca
BGOPC <bb8933400@gmail.com>
2023-04-29 22:06:14 +0330

    Added Subjects
    Added Choices to the category CharField for the syllabus

--- frontend/models.py
+++ frontend/models.py
@@ -7,7 +7,25 @@ from users.models import User
 class Booklet(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, null=False, name="Booklet Name")
-    category = models.CharField(max_length=50, null=False, default="Subject")
+    category = models.CharField(max_length=50, choices=[
+        "Math",
+        "Geometry",
+        "Physics",
+        "Biology",
+        "Chemistry",
+        "Literature",
+        "Writing",
+        "Linguistic",
+        "GeopGraphy",
+        "English",
+        "Arabic",
+        "Theology",
+        "Quran",
+        "Social Studies",
+        "Computer",
+        "Social Media Intelligence",
+        "Technology",
+    ], null=False, default="Subject")
     description = models.TextField(null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     booklet_file = models.FileField(upload_to='booklets/')
