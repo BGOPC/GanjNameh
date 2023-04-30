75f96ab208d0c220fa60bbc467e62d6885494646
BGOPC <bb8933400@gmail.com>
2023-04-30 19:24:14 +0330

    Added Views And Basic Templates for Booklet, Journals and Journal
    Booklets Renamed to BookletsView
    
    Added frontend directory to static folder cuz there will be a lot

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView
+from django.shortcuts import get_object_or_404
+from django.views.generic import View, TemplateView
 from .models import Journal, Booklet
 
 
@@ -15,7 +15,7 @@ class HomeView(TemplateView):
         return context
 
 
-class Booklets(TemplateView):
+class BookletsView(TemplateView):
     template_name = 'frontend/Booklet.html'
     paginate_by = 5
 
@@ -29,6 +29,45 @@ class Booklets(TemplateView):
         booklets = page_obj.object_list
         context['booklets'] = booklets
         context['page_object'] = page_obj
+        return context
+
+
+class BookletView(TemplateView):
+    template_name = "frontend/Booklet.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        booklet_id = kwargs.get('booklet_id', -1)
+        context['booklet'] = get_object_or_404(Booklet, id=booklet_id)
+        return context
+
+
+class JournalsView(TemplateView):
+    template_name = 'frontend/Journals.html'
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        all_journals = Journal.objects.all().order_by('-id')
+        paginator = Paginator(all_journals, self.paginate_by)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        booklets = page_obj.object_list
+        context['journals'] = booklets
+        context['page_object'] = page_obj
+        return context
+
+
+class JournalView(TemplateView):
+    template_name = "frontend/Journal.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        journal_id = kwargs.get('journal_id', -1)
+        context['journal'] = get_object_or_404(Journal, id=journal_id)
         return context
 
 
