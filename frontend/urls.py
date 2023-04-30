21c1140a6c2dbd8eca3f0f2f7717df9ad0293ca5
BGOPC <bb8933400@gmail.com>
2023-04-30 19:31:34 +0330

    added url strructure for new views

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
-    path("support", views.SupportView.as_view(), name="support")
+    path("Booklets", views.BookletsView.as_view(), name="booklets-page"),
+    path("Booklets/Booklet/<int:booklet_id>", views.BookletsView.as_view(), name="booklet-page"),
+    path("Journals", views.JournalsView.as_view(), name="journals-page"),
+    path("Booklets/Booklet/<int:journal_id>", views.JournalView.as_view(), name="journal-page"),
+    path("support", views.SupportView.as_view(), name="support"),
 ]
