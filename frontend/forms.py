0d1557159b094dbbe732d7873a08aa1b99c6a76b
BGOPC <bb8933400@gmail.com>
2023-05-01 17:30:50 +0330

    Edited Contact Form, Basic UploadBookletView
    Added widget and basic attrs to teh contactform fields

--- frontend/forms.py
+++ frontend/forms.py
@@ -2,5 +2,19 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField()
-    message = forms.CharField(widget=forms.Textarea)
+    name = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "",
+        "placeholder": "Enter Your Email",
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        "class": "",
+        "placeholder": "Enter Your Email",
+    }))
+    message = forms.CharField(widget=forms.Textarea(attrs={
+        "class": "",
+        "placeholder": "Enter Your Massage",
+    }))
+
+
+class UploadBookletForm(forms.Form):
+    pass
